954e241f1ccb0009debe28af90407d93bba6351e
emidiosouza <emidiodmsouza@gmail.com>
2024-01-25 15:12:05 -0300

    Update conselheiro.py

--- conselheiro.py
+++ conselheiro.py
@@ -27,6 +27,10 @@ html, body, [class*="st-"], h1, h2, h3, h4, h5, h6 {
 h1, h2, h3, h4, h5, h6 {
     color: #ffffff;
 }
+
+.caption {
+        color: #ffffff;  # Você pode substituir esta cor pelo código da cor desejada
+    }
             
 }
                   
