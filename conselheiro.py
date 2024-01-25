f29bb83b0c23a3a3f9907ae57c7dd362f5e416a1
emidiosouza <emidiodmsouza@gmail.com>
2024-01-25 19:18:12 -0300

    Update conselheiro.py

--- conselheiro.py
+++ conselheiro.py
@@ -60,7 +60,7 @@ st.sidebar.caption(st.session_state.session_id)
 col1, col2, col3 = st.columns([7, 2, 6])
 with col2:
     st.image('https://kukac.com.br/wp-content/uploads/20220125151123.png', width=40)   
-st.markdown("<h3 style='text-align: center; color: black;'>Como posso ajudar hoje, Kukacker?</h3>", unsafe_allow_html=True)
+st.markdown("<b3 style='text-align: center; color: black;'>Como posso ajudar hoje, Kukacker?</b>", unsafe_allow_html=True)
 st.divider()
 
 audio_on = st.toggle('ÁUDIO')
